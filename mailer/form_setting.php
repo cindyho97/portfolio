47836bbd80a89f8e58f5b463fb029ad555f3df47
cindyho97 <cindy.ho8197@gmail.com>
2017-09-16 14:12:45 +0200

    Adjust contact form settings

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
+	$subj = "New message from your github portfolio site!"; //letter subject
+	$to = 'cindy.ho8197@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$fromName = 'Cindy Ho'; // Your company name
 	$charset = 'UTF-8';
 ?>
