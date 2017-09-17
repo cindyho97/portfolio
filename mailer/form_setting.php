fd689777d512a621572b5132a80eff4c0e1506ff
cindyho97 <cindy.ho8197@gmail.com>
2017-09-17 16:53:30 +0200

    Add resume link

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -2,8 +2,7 @@
 	/*Form settings*/
 	$subj = "New message from your github portfolio site!"; //letter subject
 	$to = 'cindy.ho8197@gmail.com'; // Enter Your E-mail
-	//$from = 'admin@you-site-name.com'; // Admin e-mail
-    $from = 'cindy.ho8197@gmail.com'; // Admin e-mail
+	$from = 'admin@cindyho97.github.io/portfolio/'; // Admin e-mail
 	$fromName = 'Portfolio Cindy Ho'; // Your company name
 	$charset = 'UTF-8';
 ?>
