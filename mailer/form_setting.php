c9b63ab00418e05e6d251ffba1d2b37e8f177239
cindyho97 <cindy.ho8197@gmail.com>
2017-09-17 15:00:09 +0200

    Adjust size from works

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -2,7 +2,8 @@
 	/*Form settings*/
 	$subj = "New message from your github portfolio site!"; //letter subject
 	$to = 'cindy.ho8197@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Cindy Ho'; // Your company name
+	//$from = 'admin@you-site-name.com'; // Admin e-mail
+    $from = 'cindy.ho8197@gmail.com'; // Admin e-mail
+	$fromName = 'Portfolio Cindy Ho'; // Your company name
 	$charset = 'UTF-8';
 ?>
