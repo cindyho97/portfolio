9b59f21aae6b1de0abb3fe35d7a5c868938ca534
cindyho97 <cindy.ho8197@gmail.com>
2017-09-20 18:26:50 +0200

    Without php files

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from your github portfolio site!"; //letter subject
-	$to = 'https://formspree.io/cindy.ho8197@gmail.com'; // Enter Your E-mail
-	$from = 'admin@cindyho97.github.io'; // Admin e-mail
-	$fromName = 'Portfolio Cindy Ho'; // Your company name
-	$charset = 'UTF-8';
-?>
